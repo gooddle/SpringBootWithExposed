6e0842c63321abbc67bd8813f0b0b4c2d7774093
goodji <vptl7211@naver.com>
2025-02-09 16:57:24 +0900

    [Feat]: start project

---
+++ src/test/kotlin/org/example/exposed/ExposedApplicationTests.kt
@@ -0,0 +1,13 @@
+package org.example.exposed
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class ExposedApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
