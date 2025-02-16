6c02909298d3027faa9e5f9c96517b4af89a8927
goodji <vptl7211@naver.com>
2025-02-16 20:25:57 +0900

    [Feat]: 테스트 의존성 제거

--- build.gradle.kts
+++ build.gradle.kts
@@ -18,9 +18,7 @@ repositories {
 	mavenCentral()
 }
 
-val kotestVersion = "5.5.5" // 추가!
 
-val mockkVersion = "1.13.8" // 추가!
 dependencies {
 	implementation("org.springframework.boot:spring-boot-starter-web")
 	implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
