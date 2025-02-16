92589ce16b3718b54dcbdcabc5438f584ba9e643
goodji <vptl7211@naver.com>
2025-02-16 20:25:31 +0900

    [Feat]: UserRepository, UserService, UserController 수정

---
+++ src/main/kotlin/org/example/exposed/domain/user/controller/UserController.kt
@@ -0,0 +1,24 @@
+package org.example.exposed.domain.user.controller
+
+import org.example.exposed.domain.user.dto.SignUpRequest
+import org.example.exposed.domain.user.dto.SignUpResponse
+import org.example.exposed.domain.user.service.UserService
+import org.springframework.http.HttpStatus
+import org.springframework.http.ResponseEntity
+import org.springframework.web.bind.annotation.PostMapping
+import org.springframework.web.bind.annotation.RequestBody
+import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RestController
+
+@RestController
+@RequestMapping("/users")
+class UserController(
+    private val userService: UserService
+) {
+    @PostMapping("/signup")
+    fun signUp(
+        @RequestBody request: SignUpRequest
+    ):ResponseEntity<SignUpResponse> {
+        return ResponseEntity.status(HttpStatus.CREATED).body(userService.signUp(request))
+    }
+}
--- src/main/kotlin/org/example/exposed/domain/user/service/UserService.kt
+++ src/main/kotlin/org/example/exposed/domain/user/service/UserService.kt
@@ -1,16 +1,33 @@
 package org.example.exposed.domain.user.service
 
+
+import java.time.LocalDateTime
 import org.example.exposed.domain.user.dto.SignUpRequest
 import org.example.exposed.domain.user.dto.SignUpResponse
+import org.example.exposed.domain.user.model.User
 import org.example.exposed.domain.user.repository.UserRepository
+import org.springframework.security.crypto.password.PasswordEncoder
 import org.springframework.stereotype.Service
+import org.springframework.transaction.annotation.Transactional
 
 @Service
-class UserServic(
+class UserService(
     private val userRepository: UserRepository,
-) {
+    private val passwordEncoder: PasswordEncoder,
+    ) {
 
+    @Transactional
     fun signUp(request: SignUpRequest): SignUpResponse {
-        TODO()
+
+        if (userRepository.findByEmail(request.email) != null) {
+            throw IllegalArgumentException("User already exists")
+        }
+        val user = User.new {
+            username = request.username
+            email = request.email
+            password = passwordEncoder.encode(request.password)
+            createdAt = LocalDateTime.now()
+        }
+    return SignUpResponse.from(user)
     }
 }
