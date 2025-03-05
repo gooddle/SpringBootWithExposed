ca5bfdd1d011a804844427c37745bfb21b4b6db7
goodji <vptl7211@naver.com>
2025-03-05 21:57:09 +0900

    [Feat]: feed user 매니투원 관계 생성

--- src/main/kotlin/org/example/exposed/domain/feed/model/Feed.kt
+++ src/main/kotlin/org/example/exposed/domain/feed/model/Feed.kt
@@ -1,12 +1,14 @@
 package org.example.exposed.domain.feed.model
 
 import org.example.exposed.domain.feed.table.FeedTable
+import org.example.exposed.domain.user.model.User
 import org.jetbrains.exposed.dao.LongEntity
 import org.jetbrains.exposed.dao.LongEntityClass
 import org.jetbrains.exposed.dao.id.EntityID
 
 class Feed(id: EntityID<Long>) : LongEntity(id) {
     companion object : LongEntityClass<Feed>(FeedTable)
+    var creator by User referencedOn FeedTable.creatorId
     var creatorId  by FeedTable.creatorId
     var title by FeedTable.title
     var content by FeedTable.content
